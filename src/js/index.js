6f3fc00b5f659a837b82aca4a2e71381d4c090c4
Jonathan Hanson <jonathan@exabyte.io>
2021-03-31 15:34:54 -0700

    Expose the trees through a getter function, to support more variety in the fetching parameters latter

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,5 @@
 export ALL_INPUT_TEMPLATES from "./assets";
-
+export {getAppTree} from "./tree"
 export allowedResults from "./allowed_results";
 export allowedMonitors from "./allowed_monitors";
 
-export espresso_tree from "./espresso/tree";
-export jupyterlab_tree from "./jupyterLab/tree";
-export ml_tree from "./ml/tree";
-export nwchem_tree from "./nwchem/tree";
-export python_tree from "./python/tree";
-export shell_tree from "./shell/tree";
-export vasp_tree from "./vasp/tree";
---
+++ src/js/tree.js
@@ -0,0 +1,29 @@
+import ESPRESSO_TREE from "./espresso/tree";
+import JUPYTERLAB_TREE from "./jupyterLab/tree";
+import ML_TREE from "./ml/tree";
+import NWCHEM_TREE from "./nwchem/tree";
+import PYTHON_TREE from "./python/tree";
+import SHELL_TREE from "./shell/tree";
+import VASP_TREE from "./vasp/tree";
+
+const APP_TREES = {
+    'espresso': ESPRESSO_TREE,
+    'jupyterLab': JUPYTERLAB_TREE,
+    'ml': ML_TREE,
+    'nwchem': NWCHEM_TREE,
+    'python': PYTHON_TREE,
+    'shell': SHELL_TREE,
+    'vasp': VASP_TREE,
+};
+
+/**
+ * Given an application name, return the applications's tree.
+ * @param appName {str}
+ * @returns {object}
+ */
+export function getAppTree(appName) {
+    if (!(appName in APP_TREES)) {
+        throw `${appName} is not a known application with a tree.`;
+    }
+    return APP_TREES[appName];
+}
