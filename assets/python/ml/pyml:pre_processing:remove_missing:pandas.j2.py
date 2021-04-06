50603e4c79c11fa1f8e179d6adcc0c70a32b4df3
James Dean <james@exabyte.io>
2021-04-05 19:02:09 -0700

    Add pandas remove_missing unit

---
+++ assets/python/ml/pyml:pre_processing:remove_missing:pandas.j2.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------- #
+#                                                                   #
+#   Pandas Remove Duplicates workflow unit                          #
+#                                                                   #
+#   This workflow unit drops all duplicate rows, if it is running   #
+#   in the "train" mode.                                            #
+# ----------------------------------------------------------------- #
+
+
+import pandas
+import settings
+
+with settings.context as context:
+    # Train
+    if settings.is_workflow_running_to_train:
+        target = context.load("target")
+        descriptors = context.load("descriptors")
+
+        df = pandas.DataFrame(target, columns=["target"])
+        df = df.join(pandas.DataFrame(descriptors))
+
+        df = df.dropna()
+
+        target = df.pop("target").to_numpy()
+        target = target.reshape(-1,1)
+
+        descriptors = df.to_numpy()
+
+        context.save(target, "target")
+        context.save(descriptors, "descriptors")
+
+    # Predict
+    else:
+        pass
