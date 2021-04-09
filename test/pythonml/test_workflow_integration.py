245974a0937f9967d81f18d7b602c88aeb8f3070
James Dean <james@exabyte.io>
2021-04-08 17:45:52 -0700

    Add detailed documentation to the test subclasses

--- test/pythonml/test_workflow_integration.py
+++ test/pythonml/test_workflow_integration.py
@@ -81,6 +81,19 @@ def custom_name_func(test_names: List[str]) -> Callable:
 
 
 class BasePythonMLTest(unittest.TestCase):
+    """
+    Base class for running tests. The general flow of the tests here is:
+    1. Setup
+    2. Simulate training workfow
+    3. Convert training workflow to a predict workflow
+    4. Simulate predict workflow
+    5. Cleanup
+
+    Attributes:
+        category (str): The type of test this is. Used for controlling what type of training and predict set to
+                        copy in during the setUp method. Currently "regresion", "classification", and "clustering"
+                        are supported.
+    """
     category = ""
 
     def setUp(self) -> None:
@@ -178,17 +191,51 @@ class BasePythonMLTest(unittest.TestCase):
 
 
 class TestRegression(BasePythonMLTest):
+    """
+    Class to test regression jobs. Any test inside of integration_configuration.yaml with the category of "regression"
+    winds up here.
+    Currently, we hardcode in the name of the plot that might be generated (regression jobs generate a parity plot), as
+    well as the name of the unit that generates it. Here, "POS_plotParity" will generate a file named
+    "my_parity_plot.png." If the plot_unit is present and the file name in plot_name is not generated, the test will
+    end in failure.
+    """
     category = "regression"
     plot_name = "my_parity_plot.png"
     plot_unit = "POS_plotParity"
 
     @parameterized.expand(tests_regression, testcase_func_name=custom_name_func(names_regression),
                           skip_on_empty=True)
-    def test_workflows(self, *units_in_test):
+    def test_workflows(self, *units_in_test: List[str]):
+        """
+        Stub method for parameterized tests. Calls the run_tests method of the base testing class with a list of units.
+
+        Args:
+            *units_in_test (list): A list of units in the test, with the same names as those defined in
+                                   "unit_shortnames" in integration_configuration.yaml. For example, a simulated
+                                   workflow of "IO_readCSV" followed by "REG_lasso" would pass in a list as
+                                   ["IO_readCSV", "REG_lasso"]
+
+        Notes:
+            The tests are parameterized using parameterized.expand. The "tests_regression" variable above contains a
+            list of all the groups of units_in_test (this is a list of lists of strings).
+
+            The custom_name_func will give the test a name containing the test name defined in
+            integration_configuration.yaml. For example, if the test was named "Reg_ReadCSV_TrainTest_MinMax_MLP_Parity"
+            then the resulting test would be named "test_workflows_Reg_ReadCSV_TrainTest_MinMax_MLP_Parity."
+
+        """
         self.run_tests(units_in_test)
 
 
 class TestClassification(BasePythonMLTest):
+    """
+    Class to test classification jobs. Any test inside of integration_configuration.yaml with the category of
+    "classification" winds up here.
+    Currently, we hardcode in the name of the plot that might be generated (classification jobs generate a parity plot),
+    as well as the name of the unit that generates it. Here, "POS_plotROC" will generate a file named
+    "my_roc_curve.png." If the plot_unit is present and the file name in plot_name is not generated, the test will
+    end in failure.
+    """
     category = "classification"
     plot_name = "my_roc_curve.png"
     plot_unit = "POS_plotROC"
@@ -200,6 +247,14 @@ class TestClassification(BasePythonMLTest):
 
 
 class TestClustering(BasePythonMLTest):
+    """
+    Class to test clustering jobs. Any test inside of integration_configuration.yaml with the category of "clustering"
+    winds up here.
+    Currently, we hardcode in the name of the plot that might be generated (clustering jobs generate a parity plot), as
+    well as the name of the unit that generates it. Here, "POS_plotClust" will generate a file named
+    "my_clusters.png." If the plot_unit is present and the file name in plot_name is not generated, the test will
+    end in failure.
+    """
     category = "clustering"
     plot_name = "my_clusters.png"
     plot_unit = "POS_plotClust"
