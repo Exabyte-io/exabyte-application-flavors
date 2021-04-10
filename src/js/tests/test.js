677a6ce1113ae6af357de484611f9b07e5db37cb
Radik Fattakhov <radikft@gmail.com>
2021-04-10 21:09:37 +0300

    Fix tests run

--- src/js/tests/test.js
+++ src/js/tests/test.js
@@ -1,36 +1,39 @@
 import assert from "assert"
 import chai from "chai"
-import {getAllAppTemplates, getAppTree, allowedResults, allowedMonitors} from "../index";
+import { getAllAppTemplates, getAppTree, allowedResults, allowedMonitors } from "../index";
 
-describe('allowedResults is not an empty list', () => {
-    assert(Array.isArray(allowedResults))
-    assert(allowedResults.length > 0)
-});
 
-describe('allowedMonitors is not an empty object', () => {
-    assert(allowedMonitors instanceof Object)
-    assert(Object.keys(allowedMonitors).length > 0)
-});
-
-describe('Every element in ALL_INPUT_TEMPLATES is an object with the required keys', () => {
-    const ALL_INPUT_TEMPLATES = getAllAppTemplates()
-    assert(Array.isArray(ALL_INPUT_TEMPLATES))
-    assert(ALL_INPUT_TEMPLATES.length > 0)
-    ALL_INPUT_TEMPLATES.forEach( function(template, index) {
-        assert('applicationName' in template)
-        assert('executableName' in template)
-        assert('name' in template)
-        assert('content' in template)
+describe('Application Flavors', () => {
+    it('allowedResults is not an empty list', () => {
+        assert(Array.isArray(allowedResults))
+        assert(allowedResults.length > 0)
     });
-});
 
-describe('getAppTree returns results', () => {
-    const tree = getAppTree("nwchem")
-    assert('nwchem' in tree)
-});
+    it('allowedMonitors is not an empty object', () => {
+        assert(allowedMonitors instanceof Object)
+        assert(Object.keys(allowedMonitors).length > 0)
+    });
 
-describe('getAppTree raises on unknown application', () => {
-    chai.expect(
-        () => {getAppTree("unknown_app")}
+    it('Every element in ALL_INPUT_TEMPLATES is an object with the required keys', () => {
+        const ALL_INPUT_TEMPLATES = getAllAppTemplates()
+        assert(Array.isArray(ALL_INPUT_TEMPLATES))
+        assert(ALL_INPUT_TEMPLATES.length > 0)
+        ALL_INPUT_TEMPLATES.forEach(function (template, index) {
+            assert('applicationName' in template)
+            assert('executableName' in template)
+            assert('name' in template)
+            assert('content' in template)
+        });
+    });
+
+    it('getAppTree returns results', () => {
+        const tree = getAppTree("nwchem")
+        assert('nwchem' in tree)
+    });
+
+    it('getAppTree raises on unknown application', () => {
+        chai.expect(
+            () => { getAppTree("unknown_app") }
         ).to.throw("unknown_app is not a known application with a tree.");
+    });
 });
