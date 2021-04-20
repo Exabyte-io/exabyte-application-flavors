f41aa7400dc491f8afb60471b670d2c7b746c5dc
James Dean <james@exabyte.io>
2021-04-20 12:49:01 -0700

    Create model proof of concept model templates

---
+++ assets/python/ml/templates/model.pyi
@@ -0,0 +1,101 @@
+{#- ========== General setup ========== -#}
+{%- if category == clustering -%}
+    {% set result_ending = "label" %}
+{%- else -%}
+    {% set result_ending = "predictions" %}
+{%- endif -%}
+
+{#- ========== Comment Block ========== -#}
+# {{ description | safe }}
+
+{# ========== Imports Block ========== #}
+{%- for item in  imports -%}
+import {{ item }}
+{% endfor -%}
+import sklearn.metrics
+import numpy as np
+import settings
+
+with settings.context as context:
+    # Train
+    if settings.is_workflow_running_to_train:
+        {# ========== Load Training Data ========== -#}
+        # Restore the data
+        {%- if category != "clustering" %}
+        train_target = context.load("train_target")
+        test_target = context.load("test_target")
+        {% endif -%}
+        train_descriptors = context.load("train_descriptors")
+        test_descriptors = context.load("test_descriptors")
+
+        # Flatten the targets
+        train_target = train_target.flatten()
+        test_target = test_target.flatten()
+
+        {# ========== Create and Train the Model ========== -#}
+        # Initialize the model
+        model = {{ model_class }}(
+                {%- for var, arg in default_args.items() %}
+                {{ var }}={{ arg | convert_nonetype | quoted_strings | safe }},
+                {%- endfor %}
+        )
+
+        # Train the model and save
+        model.fit(train_descriptors{% if category != clustering %}, train_target{% endif %})
+        context.save(model, {{ name | quoted_strings | safe }})
+        train_{{ result_ending }} = model.predict(train_descriptors)
+        test_{{ result_ending }} = model.predict(test_descriptors)
+
+        {# ========== Regression Training Metrics ========== -#}
+        {% if category == "regression" -%}
+        # Scale predictions so they have the same shape as the saved target
+        train_predictions = train_predictions.reshape(-1, 1)
+        test_predictions = test_predictions.reshape(-1, 1)
+
+        # Scale for RMSE calc on the test set
+        target_scaler = context.load("target_scaler")
+
+        # Unflatten the target
+        test_target = test_target.reshape(-1, 1)
+        y_true = target_scaler.inverse_transform(test_target)
+        y_pred = target_scaler.inverse_transform(test_predictions)
+
+        # RMSE
+        mse = sklearn.metrics.mean_squared_error(y_true, y_pred)
+        rmse = np.sqrt(mse)
+        print(f"RMSE = {rmse}")
+        context.save(rmse, "RMSE")
+
+        {# ========== Classification Training Metrics ========== -#}
+        {% elif category == "classification" -%}
+        # Save the probabilities of the model
+        test_probabilities = model.predict_proba(test_descriptors)
+        context.save(test_probabilities, "test_probabilities")
+
+        # Print some information to the screen for the regression problem
+        confusion_matrix = sklearn.metrics.confusion_matrix(test_target,
+                                                            test_predictions)
+        print("Confusion Matrix:")
+        print(confusion_matrix)
+        context.save(confusion_matrix, "confusion_matrix")
+
+        {% endif -%}
+
+        {# ========== Save the data ========== -#}
+        context.save(train_{{ result_ending }}, "train_{{ result_ending }}")
+        context.save(test_{{ result_ending }}, "test_{{ result_ending }}")
+
+    # Predict
+    else:
+        {# ========== Predictions Block ========== -#}
+        # Restore data
+        descriptors = context.load("descriptors")
+
+        # Restore model
+        model = context.load({{ name | quoted_strings | safe }})
+
+        # Make some predictions
+        predictions = model.predict(descriptors)
+
+        # Save the predictions to file
+        np.savetxt("predictions.csv", predictions, header="prediction", comments="", fmt="%s")
