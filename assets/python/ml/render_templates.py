0b58d5247d518b03a0dd497f99d597aa45c01b53
James Dean <james@exabyte.io>
2021-04-20 14:33:11 -0700

    Clean up templates, add docstrings, guard main rendering routine behind an `if __name__...` statement.

--- assets/python/ml/render_templates.py
+++ assets/python/ml/render_templates.py
@@ -1,11 +1,20 @@
+from typing import Any
+
 import jinja2.ext
 import yaml
 import black
 
-loader = jinja2.FileSystemLoader('templates/')
 
+def quoted_strings(value: Any) -> Any:
+    """
+    Filter to ensure strings are surrounded in quotes in Jinja templating.
 
-def quoted_strings(value):
+    Args:
+        value: The value to be adjusted.
+
+    Returns:
+        A string wrapped in quotes, if a string was passed in. Otherwise none.
+    """
     if isinstance(value, str):
         result = f'"{value}"'
     else:
@@ -13,7 +22,18 @@ def quoted_strings(value):
     return result
 
 
-def convert_nonetype(value):
+def convert_nonetype(value: Any) -> Any:
+    """
+    Filter for converting the string "None" to the None literal in Jinja templating.
+    Note that the string is converted to title-case before being tested, so any variation of casing works too, such as
+    "none," "NONE," "NoNe," etc.
+
+    Args:
+        value: The value to be adjusted.
+
+    Returns:
+        A literal None if "None" was passed in, otherwise returns the value that was passed in.
+    """
     if isinstance(value, str) and value.title() == "None":
         result = None
     else:
@@ -21,28 +41,35 @@ def convert_nonetype(value):
     return result
 
 
-env = jinja2.Environment(autoescape=True, loader=loader)
-env.filters['quoted_strings'] = quoted_strings
-env.filters['convert_nonetype'] = convert_nonetype
-env.add_extension(jinja2.ext.do)
+if __name__ == "__main__":
+    # Tell Jinja where our templates are located
+    loader = jinja2.FileSystemLoader('templates/')
 
-template = env.get_template('model.pyi')
+    # Populate the Jinja environment with our defined filters
+    env = jinja2.Environment(autoescape=True, loader=loader)
+    env.filters['quoted_strings'] = quoted_strings
+    env.filters['convert_nonetype'] = convert_nonetype
+    env.add_extension(jinja2.ext.do)
 
-with open("models.yaml", "r") as inp:
-    models = tuple(yaml.safe_load_all(inp))
+    # Deal with model templates
+    template_type = "model"
+    template = env.get_template(f'{template_type}.pyi')
 
-for model in models:
-    filename = f"pyml:model:{model['name']}_{model['category']}:{model['provider']}.py"
-    print(filename)
-    with open(filename, "w") as outp:
-        outp.write(
-            # Ensure pep8 compliance with Black
-            black.format_str(template.render(**model),
-                             mode=black.Mode(target_versions={black.TargetVersion.PY36,
-                                                              black.TargetVersion.PY37,
-                                                              black.TargetVersion.PY38},
-                                             line_length=120,
-                                             string_normalization=False,
-                                             is_pyi=False)
-                             )
-        )
+    with open(f"{template_type}.yaml", "r") as inp:
+        models = tuple(yaml.safe_load_all(inp))
+
+    for model in models:
+        filename = f"pyml:{template_type}:{model['name']}_{model['category']}:{model['provider']}.py"
+        print(filename)
+        with open(filename, "w") as outp:
+            outp.write(
+                # Ensure pep8 compliance with Black
+                black.format_str(template.render(**model),
+                                 mode=black.Mode(target_versions={black.TargetVersion.PY36,
+                                                                  black.TargetVersion.PY37,
+                                                                  black.TargetVersion.PY38},
+                                                 line_length=120,
+                                                 string_normalization=False,
+                                                 is_pyi=False)
+                                 )
+            )
